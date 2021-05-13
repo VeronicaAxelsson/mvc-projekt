7d56a2b18597118d7a6038cdd99aae89716d626c
Your Name <veronica-a@live.se>
2021-05-13 08:07:09 +0100

    create foundation for view layout

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,22 @@
+<!-- /**
+ * Standard view template to generate a simple web page, or part of a web page.
+ */ -->
+
+<!doctype html>
+<html>
+    <meta charset="utf-8">
+    <!-- <title><?= $title ?? "No title" ?></title> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="/favicon.ico">
+    <link rel="stylesheet" href="{{ url('css/style.css') }}">
+</head>
+
+<body>
+
+<header>
+    <nav>
+        <a href="{{ url('/') }}">Home</a>
+        <a href="{{ url('/adventure') }}">Adventure</a>
+    </nav>
+</header>
+<main>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@include('includes.header')
+<h1>Index Page</h1>
+
+<p>{{ $message }}</p>
+@include('includes.footer')
